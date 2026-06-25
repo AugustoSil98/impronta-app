618a3ced24aaeeb1b64be05ef57d37efe94aa987
Augustosil98 <augustosilverio91@gmail.com>
2026-06-25 16:57:16 -0300

    Force cache refresh v4 para fix produccion

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'impronta-v3';
+const CACHE = 'impronta-v4';
 const ASSETS = ['/', '/index.html', '/app.js', '/styles.css'];
 
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
