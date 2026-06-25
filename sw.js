c9b6de8f3960c2da098c7193985410235676c8e0
Augustosil98 <augustosilverio91@gmail.com>
2026-06-25 17:38:57 -0300

    SW network-first, borra cache viejo

--- sw.js
+++ sw.js
@@ -1,5 +1,17 @@
 const CACHE = 'impronta-v5';
-const ASSETS = ['/', '/index.html', '/app.js', '/styles.css'];
 
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
